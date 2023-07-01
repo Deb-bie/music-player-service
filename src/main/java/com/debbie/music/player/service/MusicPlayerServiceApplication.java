b0c5ce2a32a61e3ff4100c395914bbef7cd75601
deborah-asamoah <deborah.asamoah@turntabl.io>
2023-07-01 10:18:27 +0000

    Initial commit

---
+++ src/main/java/com/debbie/music/player/service/MusicPlayerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.debbie.music.player.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicPlayerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicPlayerServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/debbie/music/player/service/MusicPlayerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.debbie.music.player.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicPlayerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
